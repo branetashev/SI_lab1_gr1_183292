6f8b9f8c871fd339aaad88064658d2ae675a15ef
unknown <branko.tashev@students.finki.ukim.mk>
2020-04-11 20:42:32 +0200

    student class finised

--- Student.java
+++ Student.java
@@ -6,14 +6,46 @@ class Student {
 	List<String> labPoints = new ArrayList<String>();
 
 	//TODO constructor
+    Student (string[] index, string[]firstName, string[]lastName){
+        this.index = index;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
 	//TODO seters & getters
+    void getIndex {
+        return getIndex = index;
+    }
+    void getFirstName {
+        return getFirstName = firstName;
+    }
+    void getLastName {
+        return getLastName = lastName;
+    }
+    public void setIndex(string[] index)
+    {
+        this.index = index;
+    }
+    public void setFirstName(string[] firstName)
+    {
+        this.firstName = firstName;
+    }
+    public void setLastName(string[] lastName)
+    {
+        this.lastName= lastName;
+    }
 
 	public double getAverage() {
 		//TODO
-	}
+		labPoints.for(int i=0;i<students; i++){
+        for(j=0;j<labs;j++){
+        int sum+=point;}
+        return sum/8;
+        }}
 
 	public boolean hasSignature() {
 		//TODO
+		if(point>=8)
+            return true;
 	}
 }
